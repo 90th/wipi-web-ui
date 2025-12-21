93ab2f788dac0ac5a126d5664d158193af067a01
rdbo <rdbodev@gmail.com>
2025-12-21 21:03:09 +0000

    started making proper login page

---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,18 @@
+import { JSX, ParentProps } from "solid-js"
+
+export function Card({class: userClass, children, ...rest}: JSX.IntrinsicElements["div"]) {
+  const classAttr = "bg-white border-2 border-rose-600 w-96 rounded-lg px-2" + (userClass ? " " + userClass : "");
+  return <div class={classAttr} {...rest}>
+    {children}
+  </div>
+}
+
+Card.Title = ({class: userClass, children, ...rest}: JSX.IntrinsicElements["h1"]) => {
+  const classAttr = "pt-6 pb-2 text-xl font-black text-slate-900 text-center" + (userClass ? " " + userClass : "");
+  return <h1 class={classAttr} {...rest}>{children}</h1>;
+}
+
+Card.Body = ({class: userClass, children, ...rest}: JSX.IntrinsicElements["h1"]) => {
+  const classAttr = "border-t-2 border-gray-200 px-2 pt-4 pb-8" + (userClass ? " " + userClass : "");
+  return <div class={classAttr} {...rest}>{children}</div>;
+}
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,22 +1,27 @@
 import { useNavigate } from "@solidjs/router";
 import { useAuth } from "../providers/auth";
 import { createEffect } from "solid-js";
+import { Card } from "../components/ui/Card";
+import Button from "../components/ui/Button";
 
 export default function LoginPage() {
   let passwordElement! : HTMLInputElement;
   const { signIn } = useAuth();
 
   const doSignIn = () => {
+    window.event?.preventDefault();
     const password = passwordElement.value;
     signIn({ password });
   }
 
-  return <>
-    <h1>Login Page</h1>
-    <div>
-      <label>Password:</label>
-      <input ref={passwordElement} type="password"/>
-      <button onClick={doSignIn}>Submit</button>
-    </div>
-  </>
+  return <Card>
+    <Card.Title>Login</Card.Title>
+    <Card.Body>
+      <form class="flex flex-col" onSubmit={doSignIn}>
+        <label class="text-slate-900 font-bold">Password</label>
+        <input ref={passwordElement} class="border-2 border-slate-600 rounded-md px-2" type="password" required/>
+        <Button class="my-2">Sign In</Button>
+      </form>
+    </Card.Body>
+  </Card>
 }
