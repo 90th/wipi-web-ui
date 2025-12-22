37efd41cfa24870c6282275c1276ae9628b7e9fd
rdbo <rdbodev@gmail.com>
2025-12-22 21:08:26 +0000

    added new button for theme switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,9 @@
 import { A } from "@solidjs/router";
 import { useTheme } from "../providers/theme";
 import Button from "./ui/Button";
+import ThemeButton from "./ThemeButton";
 
 export default function NavBar() {
-  const { theme, setTheme } = useTheme();
-  const switchTheme = () => {
-    if (theme() == "light")
-      setTheme("dark");
-    else {
-      setTheme("light");
-    }
-  }
   return <nav class="sticky top-0 z-50 w-full bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 shadow-sm transition-colors duration-200">
     <div class="container mx-auto px-4 h-16 flex items-center justify-between">
       <div class="flex items-center gap-3">
@@ -18,9 +11,7 @@ export default function NavBar() {
         <h1 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">WiPi Web UI</h1>
       </div>
       <div>
-        <Button onClick={switchTheme} class="text-sm">
-          {theme() === 'light' ? 'Dark Mode' : 'Light Mode'}
-        </Button>
+        <ThemeButton/>
       </div>
     </div>
   </nav>;
---
+++ src/components/ThemeButton.tsx
@@ -0,0 +1,21 @@
+import { createMemo } from "solid-js";
+import { useTheme } from "../providers/theme";
+
+export default function ThemeButton() {
+  const { theme, setTheme } = useTheme();
+  const sunImage = '/public/assets/images/sun.svg';
+  const moonImage = '/public/assets/images/moon.svg';
+  const img = createMemo(() => theme() == "dark" ? moonImage : sunImage);
+
+  const switchTheme = () => {
+    if (theme() == "light")
+      setTheme("dark");
+    else {
+      setTheme("light");
+    }
+  }
+
+  return <button onClick={switchTheme} class="cursor-pointer select-none px-2 rounded-full py-2 flex items-center justify-center border-2 border-gray-200 dark:border-gray-700">
+    <img class="h-8" src={img()}/>
+  </button>
+}
