059820fa8e8721f5da60b81005bf02f1df793eb7
rdbo <rdbodev@gmail.com>
2026-01-14 21:26:12 +0000

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
 export default function Footer() {
   return (
-    <footer class="border-t border-gray-200 bg-white py-16 dark:border-gray-800 dark:bg-gray-950 transition-colors duration-200">
+    <footer class="
+      border-t border-gray-200 bg-white py-16 transition-colors duration-200 text-gray-500
+      dark:border-gray-800 dark:bg-gray-950
+      dark:text-gray-400
+      sigma:border-sigma-bright sigma:bg-sigma-dark sigma:text-sigma-bright
+    ">
       <div class="container mx-auto flex flex-col items-center justify-between gap-2 px-4 md:h-16 md:py-0">
-        <p class="text-center text-sm leading-loose text-gray-500 dark:text-gray-400 md:text-left">
+        <p class="text-center text-sm leading-loose ">
           Copyright (C) Rdbo - 2025 - All rights reserved.
         </p>
-        <p class="text-center text-sm leading-loose text-gray-500 dark:text-gray-400 md:text-left">
-          For more information about the WiPi project, visit <a href="https://github.com/rdbo/wipi" class="font-medium underline underline-offset-4 hover:text-gray-900 dark:hover:text-gray-50">GitHub</a>
+        <p class="text-center text-sm leading-loose">
+          For more information about the WiPi project, visit <a href="https://github.com/rdbo/wipi" class="font-medium underline underline-offset-4 hover:text-gray-900 transition-colors dark:hover:text-gray-50 sigma:hover:text-white">GitHub</a>
         </p>
       </div>
     </footer>
